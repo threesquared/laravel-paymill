1b3179b53ee11211e30664215862c1eeb7f4220d
Ben Speakman <ben@d-formed.net>
2015-01-08 13:23:31 +0000

    Time to update some api keys...

--- src/config/config.php
+++ src/config/config.php
@@ -2,7 +2,7 @@
 
 return array(
 
-    'public_key' => '64577284258f4c2e833e91073520e8f0',
-    'private_key' => 'cbda57c74ee534c1b558d52f67eb59c9'
+    'public_key' => '<PUBLIC API KEY>',
+    'private_key' => '<PRIVATE API KEY>'
     
 );
